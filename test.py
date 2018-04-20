6ca3e75f7a762047cf1af221e7d71c98feb68a28
SimonGreenhill <simon@simon.net.nz>
2018-04-20 16:12:10 +0200

    update tests

--- test.py
+++ test.py
@@ -21,14 +21,19 @@ def test_sources(cldf_dataset, cldf_logger):
     assert len(cldf_dataset.sources) == 13
 
 
-# The final list of basic vocabulary includes 126 meanings (for the full data
-# set, see the Supplementary Materials presented in the online Appendix).
+# Birchall et al: "The final list of basic vocabulary includes 126 meanings 
+# (for the full data set, see the Supplementary Materials presented in the
+# online Appendix)."
 def test_parameters(cldf_dataset, cldf_logger):
-    assert len(list(cldf_dataset['ParameterTable'])) == 126
+    # actually, there are 125 meanings, the extra is because "Mother" is entered
+    # in the wordlist as "Mother" and "Mother " (note trailing space). A naive 
+    # count of the different word strings (without removing whitespace) has led
+    # to the incorrect count.
+    assert len(list(cldf_dataset['ParameterTable'])) == 125
 
 
-# A data set of 285 cognate sets of basic vocabulary was compiled using the 
-# sources presented in 2 above.
+# Birchall et al: "A data set of 285 cognate sets of basic vocabulary was 
+# compiled using the sources presented in 2 above."
 def test_cognates(cldf_dataset, cldf_logger):
     cogsets = {c['Cognateset_ID'] for c in cldf_dataset['CognateTable']}
     assert len(cogsets) == 285
